df8c499b2b4e967b07c5e56e8388f7b5adfc3605
Vadim <burebistamacovei@gmail.com>
2020-09-24 20:58:35 +0300

    Clean - fix first project
    Delete wrong files
    Prepare project for test on FCC
    Fix timeline

--- 1 Tribute Page/resources/js/script.js
+++ 1 Tribute Page/resources/js/script.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {
 
-    // $('h1').click(function() {
-    // 	$(this).css('background-color', '#ff0000');
-    // 	})
-
     /* Navigation scroll */
     // Select all links with hashes
     $('a[href*="#"]')
@@ -27,16 +23,7 @@ $(document).ready(function () {
                     $('html, body').animate({
                         scrollTop: target.offset().top - 50
                     }, 1500, function () {
-                        // Callback after animation
-                        // Must change focus!
-                        // var $target = $(target);
-                        // $target.focus();
-                        // if ($target.is(":focus")) { // Checking if the target was focused
-                        //   return false;
-                        // } else {
-                        //   $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                        //   $target.focus(); // Set focus again
-                        // };
+                        
                     });
                 }
             }
