7f928fdfc6201402ee834cea06db1cbc0051e2f1
Vadim <burebistamacovei@gmail.com>
2021-10-21 23:57:13 +0300

    #4 Project update
    Added mobile version
    Fix nav hover effect
    Added better images

--- Technical_Documentation_Page/resources/js/script.js
+++ Technical_Documentation_Page/resources/js/script.js
@@ -22,3 +22,45 @@ function removeActiveClasses() {
         link.classList.remove('active');
             })
 }
+
+// Scroll button
+
+var scrollButton = document.querySelector(".scrollUp");
+
+scrollButton.addEventListener('click', () => {
+  topFunction()
+  
+} )
+
+window.onscroll = function() {scrollFunction()}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 640 || document.documentElement.scrollTop > 640) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// Navigation
+
+const hamburger = document.querySelector('.hamburger')
+const navigation = document.querySelector('#navbar')
+const nav_links = document.querySelectorAll('.nav-link')
+
+hamburger.addEventListener('click', () => {
+  navigation.classList.toggle('active')
+})
+
+
+nav_links.forEach(nav_link => {
+nav_link.addEventListener('click', () => {
+    navigation.classList.toggle('active')
+  })
+})
+
